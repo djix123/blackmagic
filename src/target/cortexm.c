1961f0ec9e0afcefe6d0874d2c83eaa2388e42a6
Jonathan Giles <jgiles@Jonathans-Air.lan>
2021-10-06 20:17:54 -0400

    Redefine PROBE(x) to effectively NULL to reduce stlink build to minimal (ie < 64K) Will be able to use only with 'hosted'

--- src/target/cortexm.c
+++ src/target/cortexm.c
@@ -378,8 +378,9 @@ bool cortexm_probe(ADIv5_AP_t *ap)
 	} else {
 		target_check_error(t);
 	}
+
 #define PROBE(x) \
-	do { if ((x)(t)) {return true;} else target_check_error(t); } while (0)
+	do { if (0) {return true;} else target_check_error(t); } while (0)
 
 	switch (ap->ap_designer) {
 	case AP_DESIGNER_FREESCALE:
